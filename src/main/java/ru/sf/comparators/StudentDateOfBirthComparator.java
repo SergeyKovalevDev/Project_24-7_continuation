192329949ecbb2fc1dd0b594c3fa343dd406a399
Sergey Kovalev <dip.deep@mail.ru>
2022-06-13 23:17:35 +0300

    dateOfBirth type changed to LocalDate

--- src/main/java/ru/sf/comparators/StudentDateOfBirthComparator.java
+++ src/main/java/ru/sf/comparators/StudentDateOfBirthComparator.java
@@ -2,13 +2,13 @@ package ru.sf.comparators;
 
 import ru.sf.models.Student;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class StudentDateOfBirthComparator implements StudentComparatorInterface {
     @Override
     public int compare(Student o1, Student o2) {
-        LocalDateTime d1 = o1.getDateOfBirth();
-        LocalDateTime d2 = o2.getDateOfBirth();
+        LocalDate d1 = o1.getDateOfBirth();
+        LocalDate d2 = o2.getDateOfBirth();
         if (d1 == null && d2 == null) return 0;
         else if (d1 == null) return -1;
         else if (d2 == null) return 1;
